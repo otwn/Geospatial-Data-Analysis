e155af9b6112d5b75b9681d8d982a7b1ab8d0164
Shin Kobara <shin.kobara@gmail.com>
2021-11-23 17:51:18 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Geospatial-Analysis-Examples
+Geospatial Analysis with Python and/or ESRI Arcpy/ArcGIS
---
+++ latest_observation/latest_obs_gulfofmexico.py
@@ -0,0 +1,44 @@
+
+import os
+import requests
+import numpy as np
+import pandas as pd
+
+
+# download latest_obs.txt using request
+def download_latest_obs(url):
+    response = requests.get(url)
+    with open('latest_obs.txt', 'w') as f:
+        f.write(response.text)
+
+
+# read latest_obs.txt with pandas and return a dataframe
+def get_latest_obs_df(filename):
+    # read latest_obs.txt with pandas
+    stations_df = pd.read_table(filename, delim_whitespace=True,\
+        skiprows=[1])
+    # replace "MM" with np.nan
+    stations_df.replace('MM', np.nan, inplace=True)
+    # convert to numeric
+    stations_df = stations_df.apply(pd.to_numeric, errors='ignore')
+    # fiter out the station outside of the Gulf of Mexico
+    stations_df = stations_df.loc[(stations_df['LAT'] > 18.0) & (stations_df['LAT'] <= 31.0)\
+               & (stations_df['LON'] < -87.0) & (stations_df['LON'] > -100.0)]
+    
+    return stations_df
+
+
+# save dataframe to csv
+def save_df_to_csv(df, filename):
+    df.to_csv(filename, index=False)
+
+
+if __name__ == '__main__':
+    url = 'https://ndbc.noaa.gov/data/latest_obs/latest_obs.txt'
+    # download_latest_obs(url)
+
+    stations = get_latest_obs_df(url)
+    print(stations.head())
+
+    save_df_to_csv(stations, 'latest_obs_gulfofmexico.csv')
+        
